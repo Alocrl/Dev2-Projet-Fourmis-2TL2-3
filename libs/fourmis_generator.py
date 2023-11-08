7361f57b0014499d37667ac63c99c8ec74e968a6
MaximeMalpica <147186523+MaximeMalpica@users.noreply.github.com>
2023-11-08 20:11:32 +0100

    Update fourmis_generator.py

--- libs/fourmis_generator.py
+++ libs/fourmis_generator.py
@@ -1,37 +1,39 @@
 import random
 
-class Ant :
-    """class for an ant"""
-    def __init__(self,type, sexe, life) :
+class Anthill:
+    """Class for an anthill"""
+
+    def __init__(self, type):
         self.type = type
+
+class Ant(Anthill):
+    """Class for an ant"""
+
+    def __init__(self, type, sexe, life):
+        super().__init__(type)
         self.sexe = sexe
         self.life = life
 
     @property
-    def everyDayLife (self):
-        """ delete a day in the life of the ant and return true if she have less than 1 live"""
+    def everyDayLife(self):
+        """Delete a day in the life of the ant and return True if she has less than 1 life"""
         self.life -= 1
-        if self.life <= 0 :
-            return True
-        else:
-            return False
+        return self.life <= 0
 
-    def __str__(self) :
-        return f"type : {self.type} - sexe : {self.sexe} - vie : {self.life}"
-
+    def __str__(self):
+        return f"type: {self.type} - sexe: {self.sexe} - vie: {self.life}"
 
 def generateAnts(nombre_fourmis):
-    """"return a dictionary containing the objects created with the Ants class"""
-
+    """Return a dictionary containing the objects created with the Ant class"""
     objectAllAnts = {}
-    antType = random.choice(["Atta", "Camponotus", "Formica", "Formica", "Solenopsis"])
+    antTypes = ["Atta", "Camponotus", "Formica", "Formica", "Solenopsis"]
     antSexe = ["M", "F"]
-    antLife = range(23)
+    antLife = list(range(23))
 
-    for i in range(nombre_fourmis) :
+    for i in range(nombre_fourmis):
+        antType = random.choice(antTypes)
         sexe = random.choice(antSexe)
         nbrLife = random.choice(antLife)
-
         objectAllAnts[i] = Ant(antType, sexe, nbrLife)
 
     return objectAllAnts
