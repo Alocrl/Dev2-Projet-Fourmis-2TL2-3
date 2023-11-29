65f366b7f7dbd01dc59b1cfb5ea9420e0033041c
gaëtan carbonnelle <Gaetan.carbonnelle1@gmail.com>
2023-11-29 16:26:36 +0100

    harmoniser/verifier le code Food et Larva
    - faire quelques ajustement sur la classe Food et Larva
    - et puis harmoniser/simplifier le tout

--- libs/nouriture_generator.py
+++ libs/nouriture_generator.py
@@ -1,19 +1,12 @@
 import random
 import json
 
-#add a json file with food types and their values 
-try :
-    with open("data/eat_data.json") as file :
-        all_data_eat = json.load(file)
-except IOError as e :
-    print(f"IOERROR : {e}")
-
 #class to create a food
 class Food:
     def __init__(self,all_type_and_life):
         self.all_type_and_life = all_type_and_life # = quantity d'energie
     
-    def check_class(self): 
+    def check_class(self):
         if not self.all_type_and_life:
             return False
         else:
@@ -23,24 +16,31 @@ class Food:
         if not self.all_type_and_life:
             return False
         else:
-            all_key = list(self.all_type_and_life.keys())
-            first_key = all_key[0]
-            if self.all_type_and_life[first_key] <= 0 :
-                del self.all_type_and_life[first_key]
-            else :
-                self.all_type_and_life[first_key] -= 1
+            all_key = len(list(self.all_type_and_life.keys()))
+            for foodName in list(self.all_type_and_life.keys()) :
+                if self.all_type_and_life[foodName] <= 0 :
+                    del self.all_type_and_life[foodName]
+                else :
+                    self.all_type_and_life[foodName] -= 1
+                    return True
+            return False
+
             
-            if not self.all_type_and_life:
-                return False
-            else :
-                return True
-            
-    def add_food(self,nbr_collect):
+    def add_food(self, nbr_collect):
         if not self.all_type_and_life:
             return 0
-        all_key = list(self.all_type_and_life.keys())
-        first_key = all_key[0]
-        self.all_type_and_life[first_key] += all_data_eat[first_key]["nbr_life_eat"]
+
+        try:
+            with open("data/eat_data.json") as file:
+                all_data_eat = json.load(file)
+        except IOError as e:
+            print(f"IOERROR : {e}")
+        for i in range(nbr_collect) :
+            type_food = random.choice(list(all_data_eat.keys()))
+            if type_food not in self.all_type_and_life.keys():
+                self.all_type_and_life[type_food] = all_data_eat[type_food]["nbr_life_eat"]
+            else:
+                self.all_type_and_life[type_food] += all_data_eat[type_food]["nbr_life_eat"]
         
     def all_storage_food(self):
         all_food = 0
@@ -51,8 +51,18 @@ class Food:
     def __str__(self):
        return f"here's the object of the class : {self.all_type_and_life}"
 
+
+
+
 #function to generate food
 def generate_food(nbre_nouriture):
+    # add a json file with food types and their values
+    try:
+        with open("data/eat_data.json") as file:
+            all_data_eat = json.load(file)
+    except IOError as e:
+        print(f"IOERROR : {e}")
+
     all_type_and_life_object = {}
     for index in range(nbre_nouriture):  
         type_food = random.choice(list(all_data_eat.keys()))
@@ -60,6 +70,7 @@ def generate_food(nbre_nouriture):
             all_type_and_life_object[type_food] = all_data_eat[type_food]["nbr_life_eat"]
         else :
             all_type_and_life_object[type_food] += all_data_eat[type_food]["nbr_life_eat"]
+    print(all_type_and_life_object)
     allfood = Food(all_type_and_life_object)
     return allfood
 
--- main.py
+++ main.py
@@ -9,6 +9,92 @@ from libs.nouriture_generator import *
 # the program have to be launch from an command prompt
 # example of prompt : "python main.py -AW 100 -AS 10 -F 1000 -D 110 -S 0.5  DarkAnt DarkAnt DarkAnt RedAnt"
 
+def clear_screen():
+    if platform.system() == 'Windows':
+        subprocess.run("cls", shell=True)
+    else:
+        subprocess.run("clear", shell=True)
+
+
+def showState(type, nbrAntsWorkers, nbrAntssoldiers, nbrLarva, nbrFood):
+    """ show the state of the simulation """
+    print(
+        f"{type} : Il reste {nbrAntsWorkers} ouvrières, {nbrAntssoldiers} soldats, {nbrLarva} larva/eggs et {nbrFood} de nouriture ")
+
+def launchSimulation(anthill):
+    """ launch the simulation for one day of a collonie"""
+    randomDeathRate = 1 / 0.001
+
+    # delete one live to the ant and add a possible random death (workers)
+    for i in list(anthill["workers"].keys()):
+        if anthill["workers"][i].every_day_life:
+            del anthill["workers"][i]
+        if random.randrange(randomDeathRate) == 1:
+            if i in anthill["workers"]:
+                del anthill["workers"][i]
+    # delete one live to the ant and add a possible random death (soldiers)
+    for i in list(anthill["soldiers"].keys()):
+        if anthill["soldiers"][i].every_day_life:
+            del anthill["soldiers"][i]
+        if random.randrange(randomDeathRate) == 1:
+            if i in anthill["soldiers"]:
+                del anthill["soldiers"][i]
+    # delete one live to the ant (qween)
+    for i in list(anthill["qween"].keys()):
+        if anthill["qween"][i].every_day_life:
+            del anthill["qween"][i]
+
+    # update larva state + create workers if larva hatch
+    for i in list(anthill["larva"].keys()):
+        anthill["larva"][i].update_state
+        if anthill["larva"][i].every_day_life:
+            del anthill["larva"][i]
+            # ajouter une workers
+            anthill["workers"][len(anthill["workers"])] = \
+            generate_colony_workers(1, anthill["antProprety"]["worker"]["life"], anthill["type"],
+                                    anthill["antProprety"]["worker"]["nbr_food_collect_per_day"])[0]
+        if random.randrange(randomDeathRate / 10) == 1:
+            if i in anthill["larva"]:
+                del anthill["larva"][i]
+                pass
+
+    # every ant have to eat or die
+    for i in anthill["workers"].copy():
+        if anthill["food"].check_class():
+            anthill["food"].del_food()
+        else:
+            del anthill["workers"][i]
+    for i in anthill["soldiers"].copy():
+        if anthill["food"].check_class():
+            anthill["food"].del_food()
+        else:
+            del anthill["soldiers"][i]
+
+    # generate the new eggs/larva from the qween
+    if anthill["food"].all_storage_food() >= 5:
+        # make workers in function of nbr of eggs the qween can make
+        nbrLarva = len(anthill["larva"])
+        newAntLarva = generate_colony_larva(anthill["antProprety"]["qween"]["nbr_eggs_per_day"],
+                                            anthill["antProprety"]["larva"]["life"], anthill["type"])
+        for i in range(anthill["antProprety"]["qween"]["nbr_eggs_per_day"]):
+            anthill["larva"][nbrLarva] = newAntLarva[i]
+            nbrLarva += 1
+
+    # add food in fuction of the nbr of worker and the food he can collect per day
+    # anthill["food"] += len(anthill["workers"])*anthill["antProprety"]["worker"]["nbr_food_collect_per_day"]
+    nbr_to_add = len(anthill["workers"]) * anthill["antProprety"]["worker"]["nbr_food_collect_per_day"]
+    anthill["food"].add_food(nbr_to_add)
+
+    # show the state of the porgram
+    showState(anthill["type"], len(anthill["workers"]), len(anthill["soldiers"]), len(anthill["larva"]),
+              anthill["food"].all_storage_food())
+
+
+
+
+
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-AW", "--nbrAntsWorkers", help="set the number of ants at the beggin of the simulation")
@@ -21,89 +107,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
 
-    def clear_screen():
-        if platform.system() == 'Windows':
-            subprocess.run("cls", shell=True)
-        else:
-            subprocess.run("clear", shell=True)
-
-
-    def showState(type, nbrAntsWorkers, nbrAntssoldiers, nbrFood):
-        """ show the state of the simulation """
-        print(f"{type} : Il reste {nbrAntsWorkers} ouvrières, {nbrAntssoldiers} soldats et {nbrFood} de nouriture ")
-
-
-    def launchSimulation(anthill):
-        """ launch the simulation for one day of a collonie"""
-        randomDeathRate = 1 / 0.001
-
-        # delete one live to the ant and add a possible random death (workers)
-        for i in list(anthill["workers"].keys()):
-            if anthill["workers"][i].every_day_life:
-                del anthill["workers"][i]
-            if random.randrange(randomDeathRate) == 1:
-                if i in anthill["workers"]:
-                    del anthill["workers"][i]
-        # delete one live to the ant and add a possible random death (soldiers)
-        for i in list(anthill["soldiers"].keys()):
-            if anthill["soldiers"][i].every_day_life:
-                del anthill["soldiers"][i]
-            if random.randrange(randomDeathRate) == 1:
-                if i in anthill["soldiers"]:
-                    del anthill["soldiers"][i]
-        # delete one live to the ant (qween)
-        for i in list(anthill["qween"].keys()):
-            if anthill["qween"][i].every_day_life:
-                del anthill["qween"][i]
-        # create/update ants from larva
-        for i in list(anthill["larva"].keys()):
-            anthill["larva"][i].update_state
-            if anthill["larva"][i].every_day_life:
-                del anthill["larva"][i]
-                generate_colony_workers(1, anthill["antProprety"]["worker"]["life"], anthill["type"],anthill["antProprety"]["worker"]["nbr_food_collect_per_day"])
-            if random.randrange(randomDeathRate) == 1:
-                if i in anthill["larva"]:
-                    del anthill["larva"][i]
-
-        """
-        test food to eat, mange un nombre random de noruiture par jours
-        
-        """
-
-        # every ant have to eat or die
-        for i in anthill["workers"].copy():
-            if anthill["food"].check_class():
-                anthill["food"].del_food()
-            else:
-                del anthill["workers"][i]
-        for i in anthill["soldiers"].copy():
-            if anthill["food"].check_class():
-                anthill["food"].del_food()
-            else:
-                del anthill["soldiers"][i]
-
-        # generate the new ants from the qween
-        if anthill["food"].all_storage_food() >= 3:
-
-            # make workers in function of nbr of eggs the qween can make
-            nbrLarva = len(anthill["larva"])
-            newAntLarva = generate_colony_larva(anthill["antProprety"]["qween"]["nbr_eggs_per_day"], anthill["antProprety"]["larva"]["life"], anthill["type"])
-            for i in range(anthill["antProprety"]["qween"]["nbr_eggs_per_day"]):
-                anthill["larva"][nbrLarva] = newAntLarva[i]
-                nbrLarva += 1
-
-
-        # add food in fuction of the nbr of worker and the food he can collect per day
-        # anthill["food"] += len(anthill["workers"])*anthill["antProprety"]["worker"]["nbr_food_collect_per_day"]
-        nbr_to_add = len(anthill["workers"]) * anthill["antProprety"]["worker"]["nbr_food_collect_per_day"]
-        anthill["food"].add_food(nbr_to_add)
-        # show the state of the porgram
-        showState(anthill["type"], len(anthill["workers"]), len(anthill["soldiers"]),
-                  anthill["food"].all_storage_food())
 
 
     # differents types of Ants
-
     try:
         with open("data/typeAntProprety.json") as file:
             typeAntProprety = json.load(file)
@@ -131,7 +137,6 @@ if __name__ == '__main__':
 
         for i in collonies.values():
             launchSimulation(i)
-        print(collonies)
-        time.sleep(0.2)
+
         # time of sleep between every day
         time.sleep(float(args.sleepTime))
